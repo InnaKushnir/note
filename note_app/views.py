94136c3b04821ea5eaa009c1b835bf6b2f225ca6
Inna Kushnir <innakushnir63@gmail.com>
2023-11-19 11:36:50 +0200

    Permission

--- note_app/views.py
+++ note_app/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from note_app.models import Note
 from note_app.serializers import NoteSerializer, NoteCreateSerializer
@@ -12,6 +14,19 @@ class NoteViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        note = self.get_object()
+        if note.user == self.request.user or self.request.user.is_staff:
+            serializer.save()
+        else:
+            raise PermissionDenied("You are not allowed to update this note.")
+
+    def perform_destroy(self, instance):
+        if instance.user == self.request.user or self.request.user.is_staff:
+            instance.delete()
+        else:
+            raise PermissionDenied("You are not allowed to delete this note.")
 
     def get_serializer_class(self):
         if self.action == "create":
